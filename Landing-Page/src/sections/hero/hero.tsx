0c574172d3f572cd9102b4a5381aa6482b1919ff
MartaDiazV <martadioniladiazvaldebenito@gmail.com>
2026-07-23 12:41:57 -0400

    creacion de proyecto reac y creacion de carpeta

---
+++ Landing-Page/src/sections/hero/hero.tsx
@@ -0,0 +1,23 @@
+
+
+export const Hero = () => {
+  return (
+    <section id="hero">
+      <p>Hola, soy</p>
+
+      <h2>Marta Diaz Valdebenito </h2>
+
+
+      <p>
+        Desarrolladora Frontend , especializada en React,
+        TypeScript y JavaScript. Me apasiona crear aplicaciones web modernas,
+        escalables y con una excelente experiencia de usuario.
+      </p>
+
+      <div>
+        <button>Ver proyectos</button>
+        <button>Sobre mí</button>
+      </div>
+    </section>
+  );
+};
